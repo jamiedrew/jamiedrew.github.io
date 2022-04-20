e3b0df7da42a20289c6a167a03e556255d55e89d
Jamie Drew <69626055+jamiedrew@users.noreply.github.com>
2022-04-20 13:00:28 +0100

    implement tabs; change styling

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function projects() {
+  return {
+    activeProjects: 0,
+    
+    info: [
+      {
+        id: 0,
+        title: "Undra",
+        text: "Studio Decent's flagship MMO"
+      }
+    ]
+
+  }
+}
